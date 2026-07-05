8d0e633fdc4c692d0495ad42f0475eaebcb1b8f3
GustavoEmanuel901 <gustavoemanuel901>
2026-07-05 20:18:35 -0300

    Aula 6

--- dangos/views.py
+++ dangos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . import views
 # Create your views here.
+
+def index(request):
+    """Página principal do aplicativo Dango."""
+    return render(request, 'dangos/index.html')
