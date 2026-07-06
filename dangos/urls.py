740e8d95dcd1e8b55f2e8b04873fa75abba85f4e
GustavoEmanuel901 <gustavoemanuel901>
2026-07-05 21:32:54 -0300

    Aula 9 + 1

--- dangos/urls.py
+++ dangos/urls.py
@@ -22,6 +22,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics', views.topics, name='topics'),
-    path('topic/<int:topic_id>/', views.topic, name='topic'),
-
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
