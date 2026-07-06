490bcff73e1dcebb8b7f21df688ea969d59e1725
GustavoEmanuel901 <gustavoemanuel901>
2026-07-05 21:26:02 -0300

    Aula 10

--- dangos/views.py
+++ dangos/views.py
@@ -12,3 +12,10 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'dangos/topics.html', context)
+
+def topic(request, topic_id):
+    """Exibe um único tópico e todas as suas entradas."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'dangos/topic.html', context)
