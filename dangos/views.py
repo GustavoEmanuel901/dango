3f1dce2d8e3c1318248e9eced9555742c5f49d29
GustavoEmanuel901 <gustavoemanuel901>
2026-07-05 21:04:32 -0300

    Aula 8

--- dangos/views.py
+++ dangos/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from . import views
+from .models import Topic, Entry
 # Create your views here.
 
 def index(request):
     """Página principal do aplicativo Dango."""
     return render(request, 'dangos/index.html')
+
+def topics(request):
+    """Exibe todos os tópicos."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'dangos/topics.html', context)
