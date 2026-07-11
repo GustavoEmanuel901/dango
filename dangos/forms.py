5b88ffad45648df79db283b1d5eef07fe88fb431
GustavoEmanuel901 <gustavoemanuel901>
2026-07-11 18:19:52 -0300

    Aula 11

---
+++ dangos/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Topic
+
+class TopicForm(forms.ModelForm):
+    class Meta: 
+        model = Topic
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+        
--- dangos/views.py
+++ dangos/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from . import views
 from .models import Topic, Entry
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
@@ -19,3 +22,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'dangos/topic.html', context)
+
+def new_topic(request):
+    """Adiciona um novo tópico."""
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco.
+        form = TopicForm()
+    else:
+        # Dados submetidos; processa os dados.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+
+    # Exibe um formulário em branco ou inválido.
+    context = {'form': form}
+    return render(request, 'dangos/new_topic.html', context)
