eda72834502afa02cf239c54de66d859665cab92
GustavoEmanuel901 <gustavoemanuel901>
2026-07-11 21:02:06 -0300

    Aula 17

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-   # path('logout/', auth_views.LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 ]
